92cf6b815f4f95225b613dded6f4c6eb7e92f72c
Emre YILMAZ <z@emre.xyz>
2026-02-17 11:33:09 -0500

    Update .gitignore and remove build artifacts

--- dist-newstyle/tmp/src-1362433/time-compat-1.9.9/src/Data/Time/Calendar/Easter/Compat.hs
+++
@@ -1,9 +0,0 @@
-module Data.Time.Calendar.Easter.Compat (
-    sundayAfter,
-    orthodoxPaschalMoon,orthodoxEaster,
-    gregorianPaschalMoon,gregorianEaster
-    )where
-
-import Data.Time.Orphans ()
-
-import Data.Time.Calendar.Easter
--- dist-newstyle/tmp/src-1362433/time-compat-1.9.9/test/main/Test/Format/Compile.hs
+++
@@ -1,9 +0,0 @@
--- Tests succeed if module compiles
-module Test.Format.Compile
-  (
-  )
-where
-
--- broken on older GHCs or time
---    • Couldn't match representation of type: proxy t
---                               with that of: proxy (Wrapped t)
